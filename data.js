5c2da25ced4c2489866760e3c86fcc44b37b0b9a
Ashiq Hosen <142709619+Devsofficebd@users.noreply.github.com>
2025-04-17 11:33:05 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,291 @@
+const defaultBoxes = [
+
+// Tutor LMS
+{
+  "name": "Tutor LMS",
+  "description": "Here are some websites created by DevsOffice using Tutor LMS. You're going to love them — explore now!",
+  "links": [
+    "https://www.cambridgeduca.com/",
+    "https://technicaldraw.com/",
+    "https://spacedropsneakers.com/",
+    "https://sprintogrowth.com/",
+    "https://dazzlekoi.com/",
+    "https://www.chemengstudent.com/",
+    "https://surelylean.com/",
+    "https://jescalligraphy.com/",
+    "https://www.newheightscs.net/",
+    "https://seek2change.org/",
+    "https://nakedfoodmagazine.com/",
+    "https://dogmentor.fr/",
+    "https://impactadvisorygroup.net/",
+    "https://semcial.com/",
+    "https://diversityfocus.com.au/",
+    "https://pianotechniqueandmusicality.com",
+    "https://lms.learnsmith.net/",
+    "https://katiabovani.it/",
+    "https://grammarian.io/",
+    "https://victorytennisprograms.com/",
+    "https://slightwalkargument.mylearndash.com/",
+    "https://thesocialstrategist.net/",
+    "https://antenatal-training.com/",
+    "https://tasnimseminary.org/",
+    "https://orcaspickleball.org/",
+    "https://outdooradept.com/",
+    "https://your-iptv.com/en/",
+    "https://ajsrp.com/",
+    "https://contentschool.online/",
+    "https://serenityssolace.com/",
+    "https://apexspanish.com/",
+    "https://cskfoundation.org/",
+    "https://mathcircle.cskfoundation.org/",
+    "https://robotics.cskfoundation.org/",
+    "https://www.invest.com/",
+    "https://www.priromedia.com/",
+    "https://aliveandshinecenter.com/",
+    "https://socialmediauniv.com/",
+    "https://7stepsahead.online/",
+    "https://www.freetimelearning.com/",
+    "https://www.privacywatch.co.ke",
+    "http://www.washingtoncareacademy.com",
+    "https://evmasterclass.com/",
+    "https://mathematik-lernplattform.com/login/",
+    "https://circlecitystrength.com/",
+    "https://theblackwoodworldwide.com/",
+    "https://learnhub.regent.ac.za/",
+    "https://learning.ingenieurbuero-heckmann.de/",
+    "https://www.yourmindreset.com/",
+    "https://accommodationinnoosa.com.au/",
+    "https://portal.beachpropertygroup.com.au/",
+    "https://pdh.app/",
+    "https://istutoring.com/",
+    "https://www.creativegenesis.net",
+    "https://www.fullstackui.com/",
+    "https://edu.arifnotes.com",
+    "https://adcann.ca/courses",
+    "https://sixfigurecareerclub.com/",
+    "https://meyevents.com/",
+    "https://meyevents-formations.fr/",
+    "https://www.premierece.com/",
+    "https://durmancapital.com/home/",
+    "https://solstice.dedicated.co.za/ldcourses/courses/",
+    "https://thedogtrainingandlearningcenter.com/",
+    "https://whitesmoke-shrew-269640.hostingersite.com/",
+    "https://aidiscoverygroup.com/",
+    "https://membership.31palms.com/courses/",
+    "https://followingtina.com/",
+    "https://trisenclass.com/leader-membership",
+    "https://trendwiseanalytics.com/",
+    "https://taxwiseacademy.com/",
+    "https://ecodemy.ca/",
+    "https://natkeneducation.com/",
+    "https://template.coomb.co/erudite/",
+    "https://letycruz.com/",
+    "https://qhseacademy.ca/",
+    "https://uziitech.com/"
+  ]
+},
+{
+  "name": "LearnDash",
+  "description": "Explore these websites powered by LearnDash and crafted by DevsOffice. Dive in and see the impact of smart eLearning design!",
+  "links": [
+    "https://sprintogrowth.com/",
+    "https://diversityfocus.com.au/",
+    "https://victorytennisprograms.com/",
+    "https://slightwalkargument.mylearndash.com/",
+    "https://thesocialstrategist.net/",
+    "https://contentschool.online/",
+    "https://serenityssolace.com/",
+    "https://aliveandshinecenter.com/",
+    "https://socialmediauniv.com/",
+    "http://www.washingtoncareacademy.com",
+    "https://learnhub.regent.ac.za/",
+    "https://www.premierece.com/",
+    "https://solstice.dedicated.co.za/ldcourses/courses/",
+    "https://thedogtrainingandlearningcenter.com/",
+    "https://trisenclass.com/leader-membership",
+    "https://letycruz.com/",
+    "https://www.cambridgeduca.com/"
+  ]
+},
+{
+  "name": "LearnPress",
+  "description": "Here are some websites built using LearnPress by DevsOffice. Explore the smart learning experiences they offer!",
+  "links": [
+    "https://katiabovani.it/",
+    "https://mathcircle.cskfoundation.org/"
+  ]
+},
+
+{
+  "name": "Paid Memberships Pro",
+  "description": "These websites are powered by Paid Memberships Pro and built by DevsOffice. Explore how membership-based platforms are brought to life!",
+  "links": [
+    "https://grammarian.io/",
+    "https://tasnimseminary.org/",
+    "https://orcaspickleball.org/",
+    "https://www.privacywatch.co.ke",
+    "https://www.yourmindreset.com/",
+    "https://durmancapital.com/home/"
+  ]
+},
+
+{
+  "name": "MemberPress",
+  "description": "Check out these membership-based websites developed by DevsOffice using MemberPress. Powerful platforms made simple!",
+  "links": [
+    "https://aliveandshinecenter.com/",
+    "https://socialmediauniv.com/",
+    "https://www.creativegenesis.net",
+    "https://adcann.ca/courses",
+    "https://sixfigurecareerclub.com/"
+  ]
+},
+{
+  "name": "Elementor",
+  "description": "Explore these beautiful and dynamic websites designed with Elementor by DevsOffice. Modern design meets powerful performance!",
+  "links": [
+    "https://dazzlekoi.com/",
+    "https://www.chemengstudent.com/",
+    "https://surelylean.com/",
+    "https://jescalligraphy.com/",
+    "https://www.newheightscs.net/",
+    "https://seek2change.org/",
+    "https://diversityfocus.com.au/",
+    "https://pianotechniqueandmusicality.com",
+    "https://lms.learnsmith.net/",
+    "https://katiabovani.it/",
+    "https://grammarian.io/",
+    "https://victorytennisprograms.com/",
+    "https://slightwalkargument.mylearndash.com/",
+    "https://thesocialstrategist.net/",
+    "https://antenatal-training.com/",
+    "https://tasnimseminary.org/",
+    "https://orcaspickleball.org/",
+    "https://outdooradept.com/",
+    "https://your-iptv.com/en/",
+    "https://ajsrp.com/",
+    "https://apexspanish.com/",
+    "https://cskfoundation.org/",
+    "https://robotics.cskfoundation.org/",
+    "https://www.invest.com/",
+    "https://www.priromedia.com/",
+    "https://socialmediauniv.com/",
+    "https://7stepsahead.online/",
+    "https://www.freetimelearning.com/",
+    "http://www.washingtoncareacademy.com/",
+    "https://evmasterclass.com/",
+    "https://mathematik-lernplattform.com/login/",
+    "https://circlecitystrength.com/",
+    "https://theblackwoodworldwide.com/",
+    "https://learnhub.regent.ac.za/",
+    "https://www.yourmindreset.com/",
+    "https://pdh.app/",
+    "https://istutoring.com/",
+    "https://www.creativegenesis.net",
+    "https://sixfigurecareerclub.com/",
+    "https://meyevents-formations.fr/",
+    "https://www.premierece.com/",
+    "https://durmancapital.com/home/",
+    "https://solstice.dedicated.co.za/ldcourses/courses/",
+    "https://thedogtrainingandlearningcenter.com/",
+    "https://whitesmoke-shrew-269640.hostingersite.com/",
+    "https://aidiscoverygroup.com/",
+    "https://membership.31palms.com/courses/",
+    "https://followingtina.com/",
+    "https://trendwiseanalytics.com/",
+    "https://taxwiseacademy.com/",
+    "https://ecodemy.ca/",
+    "https://natkeneducation.com/",
+    "https://sprintogrowth.com/",
+    "https://template.coomb.co/erudite/",
+    "https://uziitech.com/",
+    "https://www.cambridgeduca.com/"
+  ]
+},
+{
+  "name": "MasterStudy",
+  "description": "These platforms are powered by the MasterStudy LMS plugin, providing feature-rich learning environments tailored to online education.",
+  "links": [
+    "https://lms.learnsmith.net/",
+    "https://www.privacywatch.co.ke"
+  ]
+},
+{
+  "name": "Thrive Architect",
+  "description": "This website is beautifully designed using Thrive Architect, optimized for conversions and user engagement.",
+  "links": [
+    "https://victim2victor.net/"
+  ]
+},
+{
+  "name": "WooCommerce Booking",
+  "description": "This website uses the WooCommerce Booking plugin to manage appointments and reservations seamlessly.",
+  "links": [
+    "https://serenityssolace.com/"
+  ]
+},
+
+{
+  "name": "Crocoblock",
+  "description": "These websites are built using the powerful Crocoblock suite, offering advanced functionality and design customization.",
+  "links": [
+    "https://impactadvisorygroup.net/",
+    "https://accommodationinnoosa.com.au/",
+    "https://portal.beachpropertygroup.com.au/"
+  ]
+},
+{
+  "name": "WooCommerce",
+  "description": "These websites are built using WooCommerce, offering robust e-commerce features for online stores.",
+  "links": [
+    "https://katiabovani.it/",
+    "https://mathcircle.cskfoundation.org/",
+    "https://katiabovani.it/"
+  ]
+},
+{
+  "name": "Amelia",
+  "description": "This website uses the Amelia booking plugin to manage appointments and services efficiently.",
+  "links": [
+    "https://apexspanish.com/"
+  ]
+},
+{
+  "name": "WP Courseware",
+  "description": "This website utilizes the WP Courseware plugin to create and manage online courses effectively.",
+  "links": [
+    "https://antenatal-training.com/"
+  ]
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ];
+  
